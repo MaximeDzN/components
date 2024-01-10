e2601cc476cce4d70945d9a0f2a7881917b3fb4e
MaximeDzN <maxime.dazin@gmail.com>
2024-01-10 16:08:21 +0100

    create form

--- sample.js
+++ sample.js
@@ -4,15 +4,32 @@ function buildImg(){
     }).then((res)=>{
         var html = `
             <div class="oc">
-                <img src='${res.message}'>
+                <a href="info.html"><img src='${res.message}'></a>
             </div>
         `;
         $(".oc-scroll").append(html);
     })
 }
 
-for (let index = 0; index < 100; index++) {
+function loadImage(n){
+    for (let index = 0; index < n; index++) {
+        buildImg();       
+    }
+}
+
+for (let index = 0; index < 10; index++) {
     $("#l1").append("<ul>teset</ul>")
     $("#l2").append("<ul>teset</ul>") 
     buildImg()
 }
+
+
+
+let div = $('.oc-scroll');
+
+div.scroll(() => {
+    let scr = $('.oc-scroll');
+    if (scr[0].scrollHeight - scr.scrollTop() <= scr.innerHeight() +1) {
+        loadImage(10);
+    }
+  });
